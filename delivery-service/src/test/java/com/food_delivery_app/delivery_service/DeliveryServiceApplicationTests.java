89630fbfffc8ce91484429224241b2f77ebdf40b
satyamrakhonde <satyamrakhonde@gmail.com>
2025-10-04 14:35:33 +0530

    initial structure

---
+++ delivery-service/src/test/java/com/food_delivery_app/delivery_service/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery_app.delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
